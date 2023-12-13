44d77d389e7cce7eabe525ec6b500bd0ad37c9e4
Morgan Jones <me@morganlabs.dev>
2023-12-13 11:59:58 +0000

    fix: Quit with Ctrl+Q, not just Q

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
 use std::io::{self, stdout, Read};
-
-// This is imported from the Termion crate
-// This allows us to enter Raw Mode in the terminal
 use termion::raw::IntoRawMode;
 
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        let b = b.unwrap(); // Bytecode
-        let c = b as char; // Convert bytecode to actual character
+    for byte in io::stdin().bytes() {
+        let byte = byte.unwrap();
+        let chara = byte as char;
 
-        // Test whether or not a character is a "Control Character" in ASCII
-        // Control Characters are NON-PRINTABLE, and therefore shouldn't be
-        // printed to the screen.
-        // `\r` is known as a Carraige Return. This makes sure that the next line
-        // is not indented strangely. Disabling this means that all lines are
-        // indented more and more over time. It brings the cursor back to the
-        // beginning of the line
-        if c.is_control() {
-            // In this case, we ONLY print the bytecode.
-            // When using the `format!` macro, line println! does, "{}" indicates
-            // a placeholder for a variable. Whereas {:?} indicates a placeholder
-            // for a variable that does NOT implement the Display trait. This means
-            // you can print ANYTHING.
-            println!("{:?} \r", b);
+        // Is an ASCII control character?
+        if chara.is_control() {
+            println!("{:?} \r", byte);
         } else {
-            println!("{:?} ({})\r", b, c);
+            println!("{:?} ({})\r", byte, chara);
         }
 
-        if c == 'q' {
+        // Quit with ctrl+q
+        if byte == to_ctrl_byte('q') {
             break;
         }
     }
+}
+
+// Convert the character to ctrl+character
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    return byte & 0b0001_1111;
 }
