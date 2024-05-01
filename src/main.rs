79109c081de4a16f26d1bf593228094a5e0dc551
Morgan Jones <me@morganlabs.dev>
2024-05-01 21:51:05 +0100

    fix: Error handling when failing to unwrap bytes

--- src/main.rs
+++ src/main.rs
@@ -11,23 +11,31 @@ fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
+        match b {
+            Ok(b) => {
+                let c = b as char;
 
-        // "control" characters are ASCII characters that are non-printable.
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
-        }
+                // "control" characters are ASCII characters that are non-printable.
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
 
-        if b == to_ctrl_byte('q') {
-            break;
-        }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err),
+        };
     }
 }
 
 fn to_ctrl_byte(c: char) -> u8 {
     let b = c as u8;
     return b & 0b0001_1111;
+}
+
+fn die(e: std::io::Error) {
+    panic!("{e}");
 }
