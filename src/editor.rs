be7ef6fa89a21256350e55b1a3974b68eb8d93be
Morgan Jones <me@morganlabs.dev>
2024-05-02 00:24:36 +0100

    feat: Separate reading kepresses and evaluating them

--- src/editor.rs
+++ src/editor.rs
@@ -5,7 +5,7 @@ pub struct Editor {}
 
 impl Default for Editor {
     fn default() -> Self {
-        return Self {};
+        Self {}
     }
 }
 
@@ -14,27 +14,26 @@ impl Editor {
         // Enter terminal "raw" mode
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        // "Listen" for key inputs from stdin
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Char(c) => Self::handle_character(key, c),
-                    Key::Ctrl('q') => break,
-                    _ => println!("{key:?}\r"),
-                },
-                Err(err) => Self::die(&err),
-            };
+        loop {
+            if let Err(err) = Self::process_keypress() {
+                Self::die(&err);
+            }
         }
     }
 
-    fn handle_character(key: Key, c: char) {
-        let b = c as u8;
+    fn process_keypress() -> Result<(), io::Error> {
+        let pressed_key = Self::read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => panic!("Program ended."),
+            _ => Ok(()),
+        }
+    }
 
-        // Do NOT print ASCII control characters
-        if c.is_control() {
-            println!("{b} \r");
-        } else {
-            println!("{key:?} ({b})\r");
+    fn read_key() -> Result<Key, std::io::Error> {
+        loop {
+            if let Some(key) = io::stdin().lock().keys().next() {
+                return key;
+            }
         }
     }
 
