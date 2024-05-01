f80c30de9f3e327bc6c268c6ea2039e23a4c4a30
Morgan Jones <me@morganlabs.dev>
2024-05-01 20:19:02 +0100

    feat: Quit on q<CR>

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use std::io::{self, Read};
 fn main() {
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
+        if c == 'q' {
+            break;
+        }
+
         println!("{c}");
     }
 }
