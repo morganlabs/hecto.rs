bd3770753d9ba0e8a0c618a357e55809fcf4f75c
Morgan Jones <me@morganlabs.dev>
2024-05-01 20:28:06 +0100

    feat: Enter raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
+    // This is bound to a variable, and is not being used
+    // That is because we are putting the terminal into raw mode
+    // Due to how ownership in Rust works, if something is not owned,
+    // it is removed. To keep the terminal in raw mode, this value must be
+    // owned. This has the benefit of us not having to manually unset raw
+    // mode when we leave the program!
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for b in io::stdin().bytes() {
         let c = b.unwrap() as char;
         if c == 'q' {
