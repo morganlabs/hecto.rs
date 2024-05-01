6ec4fff0d0f717ebb1be444fc90da46ca36e8520
Morgan Jones <me@morganlabs.dev>
2024-05-01 21:59:12 +0100

    feat: Use Termion to listen specifically for keys, not bytes

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,30 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+use std::io::{self, stdout};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
 
 fn main() {
-    // This is bound to a variable, and is not being used
-    // That is because we are putting the terminal into raw mode
-    // Due to how ownership in Rust works, if something is not owned,
-    // it is removed. To keep the terminal in raw mode, this value must be
-    // owned. This has the benefit of us not having to manually unset raw
-    // mode when we leave the program!
+    // Enter terminal "raw" mode
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-
-                // "control" characters are ASCII characters that are non-printable.
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-
-                if b == to_ctrl_byte('q') {
-                    break;
-                }
-            }
+    // "Listen" for key inputs from stdin
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Char(c) => handle_character(key, c),
+                Key::Ctrl('q') => break,
+                _ => println!("{:?}\r", key),
+            },
             Err(err) => die(err),
         };
     }
 }
 
-fn to_ctrl_byte(c: char) -> u8 {
-    let b = c as u8;
-    return b & 0b0001_1111;
+fn handle_character(key: Key, c: char) {
+    // Do NOT print ASCII control characters
+    if c.is_control() {
+        println!("{:?} \r", c as u8);
+    } else {
+        println!("{:?} ({})\r", key, c as u8);
+    }
 }
 
 fn die(e: std::io::Error) {
